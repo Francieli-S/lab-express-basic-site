bedf403843bb6947187299783bea7179077462ad
Francieli Sauthier <francieli.sauthier@gmail.com>
2023-04-22 13:56:35 +0200

    add style and change html structure

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express(); // app could be pizza, it's just an instance of express
 
 app.use(express.static("public"));
 
+// routes:
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/home.html");
 });
@@ -18,10 +19,6 @@ app.get("/work", (request, response) => {
 app.get("/photo-gallery", (request, response) => {
   response.sendFile(__dirname + "/views/photo-gallery.html");
 });
-
-// app.get("/users/:id", (request, response) => {
-//   response.send({ test: "a", id: request.params.id, query: request.query });
-// });
 
 app.listen(3001, () => {
   console.log("listening on port 3001");
